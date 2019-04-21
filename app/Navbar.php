d73aaab796ab6fa49cfcd5df502b148d45ac08ca
S4N70 <mr.sansan777@gmail.com>
2019-04-20 22:27:52 -0300

    Btn session
    Color rojo de cierre de session

--- app/Navbar.php
+++ app/Navbar.php
@@ -1,27 +1,7 @@
 
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-  <a class="navbar-brand" href="#"><i class="fas fa-money-check-alt"> Gmz</i>
- 
-
-
-</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-    <div class="navbar-nav">
-      <a class="nav-item nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
-      <a class="nav-item nav-link" href="#">Features</a>
-      <a class="nav-item nav-link" href="logout.php">logout</a>
-      <a class="nav-item nav-link disabled" href="#">Disabled</a>
-    </div>
-  </div>
-</nav>
-
-<!-- -->
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
             
-  <a class="navbar-brand" href="#"><i class="fas fa-money-check-alt"> </i>Gmz</a>
+  <a class="navbar-brand disabled" href="#"><i class="fas fa-money-check-alt"> </i> Gmz</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
@@ -34,21 +14,10 @@
       <li class="nav-item">
         <a class="nav-link" href="#">Link</a>
       </li>
-      <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Dropdown
-        </a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" href="#">Action</a>
-          <a class="dropdown-item" href="#">Another action</a>
-          <div class="dropdown-divider"></div>
-          <a class="dropdown-item" href="#">Something else here</a>
-        </div>
-      </li>
       <li class="nav-item">
         <a class="nav-link disabled" href="#">Disabled</a>
       </li>
     </ul>
-    <a class="nav-item nav-link" href="logout.php">X</a>
+    <a class="nav-item nav-link exit" href="logout.php">X</a>
   </div>
 </nav>
